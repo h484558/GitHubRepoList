b6f75456d8ccb212a46c2ea4221903bd02354859
h484558 <h484558@users.noreply.github.com>
2017-06-27 12:30:27 +0300

    Show installer steps depending on features selection state

---
+++ SQLConnectionValidator/CustomAction.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace SQLConnectionValidator
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult ValidateSqlConnection(Session session)
+        {
+            session.Log("Begin ValidateSqlConnection");
+
+            string dbUser = session["DB_USER"];
+            string dbPassword = session["DB_PASSWORD"];
+            string dbServer = session["DB_SERVER"];
+            string dbDatabase = session["DB_DATABASE"];
+            string rawConnectionString = "Data Source={0};Network Library=DBMSSOCN;Initial Catalog='master';User ID={2};Password={3};";
+
+            SqlConnection sqlConnection = new SqlConnection(string.Format(rawConnectionString, dbServer, dbDatabase, dbUser, dbPassword));
+
+            try
+            {
+                sqlConnection.Open();
+
+                if (new SqlCommand("SELECT db_id('" + dbDatabase + "')", sqlConnection).ExecuteScalar() != DBNull.Value)
+                {
+                    MessageBoxResult existingDbDlgResult = MessageBox.Show("Database '" + dbDatabase + "' already exists! Do you want to use it anyway?", "Database Exists", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                    if (existingDbDlgResult == MessageBoxResult.No)
+                    {
+                        session["DB_CONNECTION_SUCCESS"] = "0";
+                        return ActionResult.Success;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        new SqlCommand("CREATE DATABASE " + dbDatabase + "; DROP DATABASE " + dbDatabase + "", sqlConnection).ExecuteScalar();
+                    }
+                    catch (SqlException)
+                    {
+                        session["DB_CONNECTION_SUCCESS"] = "0";
+                        MessageBox.Show("Invalid DB name!", "Invalid DB Name", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return ActionResult.Success;
+                    }
+                    
+                }                
+            } catch (SqlException)
+            {
+                session["DB_CONNECTION_SUCCESS"] = "0";
+                MessageBox.Show("Failed to connect to DB Server!", "Connection Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return ActionResult.Success;
+            }
+
+            session["DB_CONNECTION_SUCCESS"] = "1";
+            return ActionResult.Success;
+        }
+    }
+}
